5ee0ebbcc487ff87dadadbb3d499430c11221a82
ibrahim <ibrahimagrud7@gmail.com>
2026-09-19 16:15:02 +0300

    Get min element of array completed

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Dynamic;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        public static int GetMinElementOfArray(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            int minElement = array[0];
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i] < minElement )
+                {
+                    minElement = array[i];
+                }
+      
+            }
+
+            return minElement;
+        }
+
+
+        
+
+        public static void Main()
+        {
+                int [] array={1,2,3,4,5,6};
+
+                int min=GetMinElementOfArray(array);
+
+                System.Console.WriteLine(min);
+        }
+
+    }
+}
--- SelectionSort/obj/Debug/net10.0/SelectionSort.AssemblyInfo.cs
+++ SelectionSort/obj/Debug/net10.0/SelectionSort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionSort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca4e33e24e5f76cefb40055c0d195b71a73ab02")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionSort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionSort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
