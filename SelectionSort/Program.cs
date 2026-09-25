5a330eed2d83307b46c4c9574b1069579b2dffaf
ibrahim <ibrahimagrud7@gmail.com>
2026-09-25 22:42:10 +0300

    Copleted in general way

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -17,26 +17,61 @@ namespace SelectionSort
             for (int i = 0; i < array.Length; i++)
             {
 
-                if (array[i] < minElement )
+                if (array[i] < minElement)
                 {
                     minElement = array[i];
                 }
-      
+
             }
 
             return minElement;
         }
 
 
-        
+        public static void ComputeSelectionSort(int[] array)
+        {
+            int minElement;
+            int indexOfMinElement=0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                //in this loop, we find the min element of array.
+                minElement=array[i];
+                for (int k = i; k < array.Length; k++)
+                {
+                    if (array[k] < minElement)
+                    {
+                        minElement=array[k];
+                      
+                    }
+                    
+                }
+               indexOfMinElement=array.IndexOf(minElement);
+                if(indexOfMinElement==i)
+                {
+                    break;
+                }
+               //Swap
+               ( array[i], array[indexOfMinElement])=(array[indexOfMinElement],array[i]);
+            }
+        }
+
+
 
         public static void Main()
-        {
-                int [] array={1,2,3,4,5,6};
+        {Console.Clear();
 
-                int min=GetMinElementOfArray(array);
 
-                System.Console.WriteLine(min);
+            int[] array = { 6,5,4,9,2,1};
+
+            ComputeSelectionSort(array);
+            
+            foreach (var item in array)
+            {
+                System.Console.Write(item+" ");
+            }
+        
         }
 
     }
